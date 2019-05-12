5782dbf940035c301b03e01b2f697fb64a1e198e
Zorden <valentin.pro@inbox.ru>
2019-05-12 19:36:17 +0300

    vuetify

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div id="app">
+  <nav-bar></nav-bar>
+</div>
+</template>
+
+<script>
+
+
+import NavBar from "./components/NavBar";
+export default {
+  name: 'App',
+  components: {
+    NavBar
+
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-toolbar dark class="primary">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Меню</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn
+        flat
+        v-for="(item, i) in menuItem"
+        :key="`menuitem${i}`"
+      >
+        {{ item.title}}
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+
+  export default {
+    name: "NavBar",
+    computed: {
+      menuItem() {
+        return [
+          {
+            title: 'Главная',
+          },
+          {
+            title: 'Примены работ',
+          },
+          {
+            title: 'Обратная связь',
+            route: '/w'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
