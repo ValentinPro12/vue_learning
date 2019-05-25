f2eddff019620199134519b660d5c67f516d23db
Zorden <valentin.pro@inbox.ru>
2019-05-25 18:13:38 +0300

    новый проект

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,24 +1,48 @@
 <template>
-  <v-toolbar dark class="primary">
-    <v-toolbar-side-icon></v-toolbar-side-icon>
-    <v-toolbar-title>Меню</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn
-        flat
-        v-for="(item, i) in menuItem"
-        :key="`menuitem${i}`"
-      >
-        {{ item.title}}
-      </v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
+  <v-app>
+    <v-navigation-drawer
+      absolute
+      temporary
+      v-model="drawer">
+      <v-list>
+        <v-list-tile
+          v-for="(item, i) in menuItem"
+          :key="`navdrawer${i}`"
+        >
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon
+      @click.stop="drawer = !drawer"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>Меню</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          flat
+          v-for="(item, i) in menuItem"
+          :key="`menuItem${i}`"
+        >
+          {{ item.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-app>
 </template>
 
 <script>
 
   export default {
     name: "NavBar",
+    data() {
+      return {
+        drawer: false
+      }
+    },
     computed: {
       menuItem() {
         return [
